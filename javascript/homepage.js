38e12b1b78e3cc55609d9c9c6476608fdeda7843
Johnsonec16 <43768458+Johnsonec16@users.noreply.github.com>
2019-02-26 12:23:17 -0500

    Add files via upload

---
+++ javascript/homepage.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+// for picture slideshow //
+  $("#frame").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+
+  $("#frame2").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+
+// back to top button //
+
+  $("#topFunction").click(function () {
+    $("html, body").animate({scrollTop: 0}, 500);
+  });
+
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("topFunction").style.display = "block";
+  } else {
+    document.getElementById("topFunction").style.display = "none";
+  }
+}
+
+// about me button scroll to about me section //
+
+  $("#aboutme").click(function (){
+    $('html,body').animate({
+       scrollTop: $("#practice").offset().top
+    });
+  });
+});
